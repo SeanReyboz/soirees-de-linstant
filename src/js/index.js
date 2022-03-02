6d839a5e663096fbeb214ba2f663869e53eb350f
ValentinGassend <valentin.gassend@gmail.com>
2022-03-03 00:53:06 +0100

    usable questions

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,57 @@
 $(document).ready(function () {
     var response = [];
+    var question_limit = 3;
+    var question_number = 1;
+    var PiqueScore = 0;
+    var CoeurScore = 0;
+    var TrefleScore = 0;
+    var CarreauScore = 0;
+    $(".heading div p").text(question_number + '/' + question_limit);
+
+    function getOccurrence(array, value) {
+        var count = 0;
+        array.forEach((v) => (v === value && count++));
+        return count;
+    }
+
     $(".response p").click(function () {
-        console.log($(".heading").className);
+
         var getClass = this.className;
         response.push(getClass.substring(8, 10));
         console.log(response);
-        $("._1").css({
+        $("._" + question_number).css({
             display: "block",
             visibility: "visible",
             opacity: 1,
-        })
-        $("._1").addClass("fadeIn");
+        });
+        $("._" + question_number).addClass("fadeIn");
 
+        $("._" + (question_number - 1)).css({
+            display: "none",
+            visibility: "hidden",
+            opacity: 0,
+        });
+        question_number++;
+        if (question_number <= question_limit) {
+            $(".heading div p").text(question_number + '/' + question_limit);
+        } else {
+            $(".heading div p").text(" ");
+            $(".resultat").css({
+                display: "block",
+                visibility: "visible",
+                opacity: 1,
+            });
+            $(".resultat").addClass("fadeIn");
+            PiqueScore = Math.round(((getOccurrence(response, "0")) / question_limit) * 100);
+            CoeurScore = Math.round(((getOccurrence(response, "1")) / question_limit) * 100);
+            TrefleScore = Math.round(((getOccurrence(response, "2")) / question_limit) * 100);
+            CarreauScore = Math.round(((getOccurrence(response, "3")) / question_limit) * 100);
+            $(".pique").text("pique " + PiqueScore + "%");
+            $(".coeur").text("coeur " + CoeurScore + "%");
+            $(".trefle").text("trefle " + TrefleScore + "%");
+            $(".carreau").text("carreau " + CarreauScore + "%");
+
+        }
     })
+
 });
