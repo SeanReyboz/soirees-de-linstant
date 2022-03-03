18cd4d38f838cda8ff63ed1bb88f04737948b244
ValentinGassend <valentin.gassend@gmail.com>
2022-03-03 01:47:11 +0100

    constom desc on result

--- src/js/index.js
+++ src/js/index.js
@@ -50,6 +50,59 @@ $(document).ready(function () {
             $(".coeur").text("coeur " + CoeurScore + "%");
             $(".trefle").text("trefle " + TrefleScore + "%");
             $(".carreau").text("carreau " + CarreauScore + "%");
+            if (PiqueScore > CoeurScore && PiqueScore > TrefleScore && PiqueScore > CarreauScore) {
+                $(".description").text("à tendence Pique");
+            } else if (CoeurScore > PiqueScore && CoeurScore > TrefleScore && CoeurScore > CarreauScore) {
+                $(".description").text("à tendence Coeur");
+            } else if (TrefleScore > PiqueScore && TrefleScore > CoeurScore && TrefleScore > CarreauScore) {
+                $(".description").text("à tendence Trefle");
+            } else if (CarreauScore > PiqueScore && CarreauScore > CoeurScore && CarreauScore > TrefleScore) {
+                $(".description").text("à tendence Carreau");
+            } else if (CarreauScore == PiqueScore) {
+                $(".description").text("à tendence Carreau et Pique");
+                if (CarreauScore == TrefleScore) {
+                    $(".description").text("à tendence Carreau, Trefle et Pique");
+                } else if (CarreauScore == CoeurScore) {
+                    $(".description").text("à tendence Carreau, Coeur et Pique");
+                }
+            } else if (CoeurScore == PiqueScore) {
+                $(".description").text("à tendence Carreau et Pique");
+                if (CoeurScore == TrefleScore) {
+                    $(".description").text("à tendence Coeur, Trefle et Pique");
+                } else if (CoeurScore == CarreauScore) {
+                    $(".description").text("à tendence Carreau, Coeur et Pique");
+                }
+            } else if (TrefleScore == PiqueScore) {
+                $(".description").text("à tendence Carreau et Pique");
+                if (TrefleScore == CoeurScore) {
+                    $(".description").text("à tendence Coeur, Trefle et Pique");
+                } else if (TrefleScore == CarreauScore) {
+                    $(".description").text("à tendence Carreau, Trefle et Pique");
+                }
+            } else if (TrefleScore == CoeurScore) {
+                $(".description").text("à tendence Carreau et Coeur");
+                if (TrefleScore == PiqueScore) {
+                    $(".description").text("à tendence Pique, Trefle et Coeur");
+                } else if (TrefleScore == CarreauScore) {
+                    $(".description").text("à tendence Carreau, Trefle et Coeur");
+                }
+            } else if (TrefleScore == CarreauScore) {
+                $(".description").text("à tendence Trefle et Carreau");
+                if (TrefleScore == PiqueScore) {
+                    $(".description").text("à tendence Pique, Trefle et Carreau");
+                } else if (TrefleScore == CoeurScore) {
+                    $(".description").text("à tendence Coeur, Trefle et Carreau");
+                }
+            } else if (CoeurScore == CarreauScore) {
+                $(".description").text("à tendence Coeuret Carreau");
+                if (CoeurScore == PiqueScore) {
+                    $(".description").text("à tendence Pique, Coeur et Carreau");
+                } else if (CoeurScore == TrefleScore) {
+                    $(".description").text("à tendence Coeur, Trefle et Carreau");
+                }
+            } else if (CoeurScore == CarreauScore && CoeurScore == PiqueScore && CoeurScore == TrefleScore) {
+                $(".description").text("à tendence Coeur, Trefle, Carreau et Pique");
+            }
 
         }
     })
